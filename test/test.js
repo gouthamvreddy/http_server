66f5e4db045754d0fd5809e3fb02cba8c41eb711
Goutham Reddy <gouthamvreddy@gmail.com>
2015-07-16 17:25:35 -0700

    created initial test file

---
+++ test/test.js
@@ -0,0 +1,22 @@
+var server = require('index');
+var expect = require('chai').expect;
+
+describe('http server responds to GET/POST/PUT/DELETE requests', function() {
+
+  it('should GET', function(){
+    expect();
+  });
+
+  it('should POST', function(){
+    expect();
+  });
+
+  it('should PUT', function(){
+    expect();
+  });
+
+  it('should DELETE', function(){
+    expect();
+  });
+
+});
